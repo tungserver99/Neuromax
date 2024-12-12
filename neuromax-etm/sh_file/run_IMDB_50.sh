94952985a9c17cb9bf20fa0d3d1f4f58f6a87cf9
tungserver <tunga99937@gmail.com>
2024-12-12 10:16:30 +0700

    update code etm

---
+++ neuromax-etm/sh_file/run_IMDB_50.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define the arrays for weight_GR and weight_InfoNCE
+weight_GR_values=(1 5 10 20 50)
+weight_InfoNCE_values=(1 10 30 50 80 100 130 150)
+
+# Loop through each combination of weight_GR and weight_InfoNCE
+for weight_GR in "${weight_GR_values[@]}"; do
+    for weight_InfoNCE in "${weight_InfoNCE_values[@]}"; do
+        # Run the python command with the current combination of weight_GR and weight_InfoNCE
+        python main.py --model NeuroMax --dataset IMDB --num_topics 50 --beta_temp 0.2 --num_groups 20 --epochs 500 --device cuda --lr 0.002 --lr_scheduler StepLR --dropout 0.2 --batch_size 200 --lr_step_size 125 --use_pretrainWE --weight_ECR 40 --weight_GR "$weight_GR" --alpha_ECR 20.0 --alpha_GR 5.0 --weight_InfoNCE "$weight_InfoNCE"
+    done
+done
